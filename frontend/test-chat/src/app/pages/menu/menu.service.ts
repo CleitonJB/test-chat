4310700ded58daffc95b63eb6cf71ffedde0f588
Cleiton Braga <cleitonbraga56@gmail.com>
2022-01-25 16:57:40 -0300

    Add group according by user name

--- frontend/test-chat/src/app/pages/menu/menu.service.ts
+++ frontend/test-chat/src/app/pages/menu/menu.service.ts
@@ -1,25 +1,76 @@
 import { Injectable } from '@angular/core';
+import { UserInfo } from 'src/app/models/user-info';
+import { LoginService } from '../login/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
-  private groups: any[] = [
-    {
-      name: 'Grupo 1',
-      id: '1'
-    },
-    {
-      name: 'Grupo 2',
-      id: '2'
-    },
-    {
-      name: 'Grupo 3',
-      id: '3'
-    }
-  ];
+  private groups: any[] = [];
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService
+  ) {
+    this.setGroups();
+  }
+
+  private setGroups(): void {
+    this.loginService.getCurrentUser().subscribe((currentUser: UserInfo) => {
+      switch(currentUser.userName) {
+        case 'Cleiton':
+          this.groups = [
+            {
+              id:   '1',
+              name: 'Grupo 1',
+            },
+            {
+              id:   '2',
+              name: 'Grupo 2',
+            },
+          ];
+        break;
+
+        case 'Amaro':
+          this.groups = [
+            {
+              id:   '1',
+              name: 'Grupo 1',
+            }
+          ];
+        break;
+
+        case 'Lucas':
+          this.groups = [
+            {
+              id:   '1',
+              name: 'Grupo 1',
+            },
+            {
+              id:   '2',
+              name: 'Grupo 2',
+            },
+          ];
+        break;
+
+        default:
+          this.groups = [
+            {
+              id:   '1',
+              name: 'Grupo 1',
+            },
+            {
+              id:   '2',
+              name: 'Grupo 2',
+            },
+            {
+              id:   '3',
+              name: 'Grupo 3',
+            }
+          ];
+        break;
+      }
+    });
+  }
 
   public getGroups(): any[] {
     return this.groups;
